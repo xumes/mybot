672330ee8c56dbf6f8b0f618e43165b1e49ac16b
Brian Martin <brianm1028@gmail.com>
2017-04-11 15:42:46 +0000

    initial commit

---
+++ config.js
@@ -0,0 +1,26 @@
+// User-specific configuration
+exports.conversationWorkspaceId = 'd0723074-5824-46a4-a584-66f29f95bcbb'; // replace with the workspace identifier of your conversation
+
+// Create the credentials object for export
+exports.credentials = {};
+
+// Watson Conversation
+// https://www.ibm.com/watson/developercloud/conversation.html
+exports.credentials.conversation = {
+	username: 'f5d97d85-3742-4ff5-8101-cbeeebe0e775',
+	password: '1mFGH1JeXH3t'
+};
+
+// Watson Speech to Text
+// https://www.ibm.com/watson/developercloud/speech-to-text.html
+exports.credentials.speech_to_text = {
+	password: 'G1iRAOo1pVrs',
+	username: 'd9c542e2-041f-41fa-97c0-19f51f6ad8ba'
+};
+
+// Watson Text to Speech
+// https://www.ibm.com/watson/developercloud/text-to-speech.html
+exports.credentials.text_to_speech = {
+	password: '02irRpAWGsoW',
+	username: '0bf4ac5e-9c0d-4c7e-a4b0-0710bd652647'
+};
